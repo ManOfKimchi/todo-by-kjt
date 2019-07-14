b06efe948a9308f18d630a764ca92bed61f4ca0a
ManOfKimchi <40424886+ManOfKimchi@users.noreply.github.com>
2019-07-14 18:08:40 +0900

    Updates

---
+++ precache-manifest.e1d167da9b2d1db3d4b62e6163c9714f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3640e0bc9901cf9fa031c651acc72b5f",
+    "url": "/todolist-by-kjt/index.html"
+  },
+  {
+    "revision": "e9527ccaa1600ab91387",
+    "url": "/todolist-by-kjt/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/todolist-by-kjt/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "e9527ccaa1600ab91387",
+    "url": "/todolist-by-kjt/static/js/main.554c2732.chunk.js"
+  },
+  {
+    "revision": "503b86ca0ab00c19ae77",
+    "url": "/todolist-by-kjt/static/js/runtime~main.b737434e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todolist-by-kjt/static/media/logo.5d5d9eef.svg"
+  }
+]);
